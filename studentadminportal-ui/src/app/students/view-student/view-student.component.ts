cae99e4d1e93ea102965a37a988d89e8e5ce8bd1
likhith-9 <likhithkrishna72@gmail.com>
2022-09-01 12:39:13 +0530

    sec 5

--- studentadminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import { Gender } from 'src/app/models/ui-models/gender.model';
 import { Student } from 'src/app/models/ui-models/student.model';
+import { GenderService } from 'src/app/services/gender.service';
 import { StudentService } from '../student.service';
 
 @Component({
@@ -31,10 +34,12 @@ export class ViewStudentComponent implements OnInit {
     }
   }
 
-  constructor(private readonly studentService: StudentService,
-    private readonly route: ActivatedRoute) {
+  genderList: Gender[]= [];
 
-     }
+  constructor(private readonly studentService: StudentService,
+    private readonly route: ActivatedRoute,
+    private readonly genderService: GenderService,
+    private snackbar: MatSnackBar) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
@@ -48,10 +53,31 @@ export class ViewStudentComponent implements OnInit {
               this.student = successResponse;
             }
           );
+
+          this.genderService.getGenderList()
+          .subscribe(
+            (successResponse)=> {
+                this.genderList= successResponse;
+            }
+          );
         }
       }
     );
 
   }
 
+  onUpdate(): void
+  {
+    this.studentService.updateStudent(this.student.id, this.student)
+    .subscribe(
+      (successResponse)=>{
+        this.snackbar.open('Student Updated Sucessfully', undefined, {
+          duration:2000
+        });
+      },
+      (errorResponse)=>{
+
+      }
+    );
+  }
 }
